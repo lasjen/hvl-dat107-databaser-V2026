e34e3fe6163d880c41beb161db80bdfa01ee8caf
Lasse Jenssen <lasse.jenssen@tietoevry.com>
2025-12-25 13:00:47 +0100

    Added some for video-02

---
+++ videoer/video-02/01-enkel-web-server/run-docker.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM Bygg Docker image
+echo Bygger Docker image...
+docker build -t enkel-webserver .
+
+REM Kjør container med volume mapping
+echo Starter container med volume mapping...
+docker run -d -p 8080:80 ^
+  -v "%cd%:/usr/share/nginx/html:ro" ^
+  --name webserver ^
+  enkel-webserver
+
+echo Webserver er tilgjengelig på http://localhost:8080
+echo For å stoppe containeren, kjør: docker stop webserver
+echo For å fjerne containeren, kjør: docker rm webserver
+
---
+++ videoer/video-02/02-postgresql-database/connect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# PostgreSQL connection configuration
+CONTAINER_NAME="dat107-postgres"
+DB_USER="dat107_user"
+DB_NAME="dat107_db"
+
+# Connect to PostgreSQL database
+echo "Connecting to PostgreSQL database '$DB_NAME' as user '$DB_USER'..."
+docker exec -it $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME
+
